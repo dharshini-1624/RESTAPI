5643a9bff7e2185a636d1808aaa76bbc9855e10a
dharshini-1624 <94008379+dharshini-1624@users.noreply.github.com>
2024-09-22 18:10:42 +0530

    Update import base64.py

--- import base64.py
+++ import base64.py
@@ -1,8 +1,6 @@
 import base64
 
-# Replace 'your_file.png' with the path of the file you want to convert
 with open("C:/Users/saidh/flask_backendfile.jpeg", "rb") as file:
     encoded_string = base64.b64encode(file.read()).decode('utf-8')
 
-# Print the Base64 string
 print(encoded_string)
